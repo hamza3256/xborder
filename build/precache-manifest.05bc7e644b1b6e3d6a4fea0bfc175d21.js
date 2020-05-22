8343e4e0d80bb4bd857cb8c2c04302b9793bc4b1
Hamza <ramzoo3256@gmail.com>
2020-05-22 19:50:32 +0100

    deployment build

---
+++ build/precache-manifest.05bc7e644b1b6e3d6a4fea0bfc175d21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effcf32c99921442b41ccc48a6075788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "672d0b496cb2e815fddb",
+    "url": "/static/css/2.40aca083.chunk.css"
+  },
+  {
+    "revision": "eec394856e2b936efdb1",
+    "url": "/static/css/main.114ae025.chunk.css"
+  },
+  {
+    "revision": "672d0b496cb2e815fddb",
+    "url": "/static/js/2.14dd8395.chunk.js"
+  },
+  {
+    "revision": "64eb8c4953866e22b916c87b588b21ea",
+    "url": "/static/js/2.14dd8395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec394856e2b936efdb1",
+    "url": "/static/js/main.2f37a18f.chunk.js"
+  },
+  {
+    "revision": "eeb7b50892710fb77dac",
+    "url": "/static/js/runtime-main.5ad26b1a.js"
+  },
+  {
+    "revision": "ad339990e0c286e5c42715b4d70f3521",
+    "url": "/static/media/banner3.ad339990.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  }
+]);
