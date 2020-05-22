d80aa271ce281ea2f80f207e1eb18fa8ef33b496
Hamza <ramzoo3256@gmail.com>
2020-05-22 22:39:49 +0100

    fix 2.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: [' ', '.js', '.jsx'],
         alias: {
           'Components': path.resolve(__dirname, '/components')
         }      
